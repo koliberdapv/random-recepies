0f9e7b2722f4c5c59bc130c551dca48ee348a5c0
koliberdapv <canrando4567@gmail.com>
2022-06-16 22:35:08 +0300

    text skeleton added, no design yet

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const sidebar = document.querySelector('.sidebar');
 const closeBtn = document.querySelector('.close-btn');
 const btnBars = document.querySelector('.btn-bars');
+const randomBtn = document.querySelector('.random-btn');
+const asnwerContainer = document.querySelector('.answer-container');
 
 btnBars.addEventListener('click', function () {
   sidebar.classList.add('show-sidebar');
@@ -8,4 +10,8 @@ btnBars.addEventListener('click', function () {
 
 closeBtn.addEventListener('click', function () {
   sidebar.classList.remove('show-sidebar');
+});
+
+randomBtn.addEventListener('click', function () {
+  asnwerContainer.classList.toggle('hidden');
 });
