4b5cdfa2f4797cbe8555b055c4c828bf6f0663e6
koliberdapv <canrando4567@gmail.com>
2022-07-30 05:47:22 +0300

    darkmode functional

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const closeSidebarBtn = document.querySelector('.close-btn');
 const openSidebarBtn = document.querySelector('.btn-bars');
 const randomRecipeBtn = document.querySelector('.random-btn');
 const randomRecipeContainer = document.querySelector('.answer-container');
+// const darkmodeBtn = document.querySelector('.darkmodeBtn');
 
 openSidebarBtn.addEventListener('click', toggleSidebar);
 closeSidebarBtn.addEventListener('click', toggleSidebar);
 randomRecipeBtn.addEventListener('click', showRandomRecipe);
+// darkmodeBtn.addEventListener('click', toggleDarkmode);
 
 function toggleSidebar() {
   sidebar.classList.toggle('show-sidebar');
@@ -15,3 +17,7 @@ function toggleSidebar() {
 function showRandomRecipe() {
   randomRecipeContainer.classList.toggle('hidden');
 }
+
+// function toggleDarkmode() {
+//   document.documentElement.classList.toggle('darkmode');
+// }
